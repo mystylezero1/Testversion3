18fd759fdaa9cf8e1e188d96f8cb963c386c7071
mystylezero1 <mystylezero1@gmail.com>
2026-08-07 16:51:09 +0200

    Add wedding configuration to config.js

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
-
+export const CONFIG = {
+    names: {
+        bride: "Anja",
+        groom: "Dino"
+    },
+    title: "Willkommen zu unserer Hochzeit",
+    subtitle: "Finde deinen Sitzplatz",
+    expirationDate: "2026-08-30T00:00:00"
+};
